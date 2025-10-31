9c4779a39e631744e0ad6511f932cb33287accb5
krupashetty004 <ammukrupa4@gmail.com>
2025-10-31 20:34:24 +0530

    Typescript Players cards completed

--- my-next-app/next.config.ts
+++ my-next-app/next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "i.pinimg.com", pathname: "/**" },
       { protocol: "https", hostname: "images.unsplash.com", pathname: "/**" },
       { protocol: "https", hostname: "picsum.photos", pathname: "/**" },
+      { protocol: "https", hostname: "imgs.search.brave.com", pathname: "/**" },
+      { protocol: "https", hostname: "encrypted-tbn0.gstatic.com", pathname: "/**" },
+      { protocol: "https", hostname: "i.pinimg.com", pathname: "/**" },
+      { protocol: "https", hostname: "encrypted-tbn3.gstatic.com", pathname: "/**" },
     ],
   },
 };
